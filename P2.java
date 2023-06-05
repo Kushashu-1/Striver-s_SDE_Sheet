e746ed64565fdc6058b76204c373692204db3eba
KushwahaAshutosh <kushashu21@gmail.com>
2023-06-05 11:45:34 +0530

    problem 2 added

--- P2.java
+++ P2.java
@@ -1,3 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+import java.util.ArrayList;
+
 public class P2 {
-    System.out.println("Hello")
+    public static ArrayList<ArrayList<Long>> printPascal(int n) {
+        ArrayList<ArrayList<Long>> arr = new ArrayList<>();
+        ArrayList<Long> temp = new ArrayList<>();
+        temp.add(1L);
+        arr.add(temp);
+        for (int i = 1; i < n; i++) {
+            temp = new ArrayList<>();
+            temp.add(1L);
+            for (int j = 1; j < i; j++) {
+                temp.add(arr.get(i - 1).get(j - 1) + arr.get(i - 1).get(j));
+            }
+            temp.add(1L);
+            arr.add(temp);
+        }
+        return arr;
+    }
 }
